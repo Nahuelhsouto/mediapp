a5267dee74cfe0dab1c71e051ed864b347576bd8
nahuelhsouto <nahuelhsouto@gmail.com>
2021-12-07 23:03:06 -0300

    Register mod

--- App.js
+++ App.js
@@ -71,7 +71,7 @@ const App = () => {
           position: 'absolute',
           alignItems: 'center',
           justifyContent: 'center',
-          opacity: Olas,
+          opacity: Olas4517690121071324,
         }}>
         <Logo height={500} width={550} viewBox="-120 60 750 520" />
       </Animated.View>
